5ae367291e06e796ee2ff29bfb3e88bd57341a3a
thongvm316 <vominhthong1603@gmail.com>
2022-02-25 16:09:42 +0700

    first commit

---
+++ src/app/fuse-configs/navigationConfig.js
@@ -0,0 +1,46 @@
+import i18next from "i18next"
+import ar from "./navigation-i18n/ar"
+import en from "./navigation-i18n/en"
+import tr from "./navigation-i18n/tr"
+
+i18next.addResourceBundle("en", "navigation", en)
+i18next.addResourceBundle("tr", "navigation", tr)
+i18next.addResourceBundle("ar", "navigation", ar)
+
+const navigationConfig = [
+  {
+    id: "applications",
+    title: "Applications",
+    translate: "APPLICATIONS",
+    type: "group",
+    icon: "apps",
+    children: [
+      {
+        id: "example-component",
+        title: "Example",
+        translate: "EXAMPLE",
+        type: "item",
+        icon: "whatshot",
+        url: "example",
+      },
+    ],
+  },
+  // {
+  //   id: "auth",
+  //   title: "Auth",
+  //   type: "group",
+  //   icon: "verified_user",
+  //   children: [
+  //     {
+  //       id: "login",
+  //       title: "Login",
+  //       type: "item",
+  //       url: "login",
+  //       auth: authRoles.onlyGuest,
+  //       icon: "lock",
+  //     },
+  //   ],
+  // },
+]
+
+export default navigationConfig
